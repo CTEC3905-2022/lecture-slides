730cfc365b7dd7db9effa15c8d8da94cdd99308c
Graeme Stuart <ggstuart@gmail.com>
2020-12-02 17:06:47 +0000

    slideshow functionality

---
+++ slides.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const slides = document.querySelectorAll('#slides section');
+let currentSlide = 0;
+
+
+function setSlide(slide_number) {
+	let candidate = document.querySelector('#slides section.current');
+	if(candidate) {
+		candidate.classList.remove('current');
+	}
+	let mySlide = slides.item(slide_number);
+	if(mySlide) {
+		mySlide.classList.add('current');
+	}
+	current.textContent = `${slide_number + 1} of ${slides.length}`;
+}
+
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
+document.addEventListener('keydown', keyHandler);
+
+function prevSlide(ev) {
+	currentSlide--;
+	if(currentSlide < 0) {
+		currentSlide = slides.length - 1;
+	}
+	setSlide(currentSlide);
+}
+function nextSlide(ev) {
+	currentSlide++;
+	if(currentSlide >= slides.length) {
+		currentSlide = 0;
+	}
+	setSlide(currentSlide);
+}
+
+function keyHandler(ev) {
+	console.log(ev.key);
+	switch (ev.key) {
+		case "ArrowRight":
+			nextSlide();
+			break;
+		case "ArrowLeft":
+			prevSlide();
+			break;
+
+	}
+}
+
+
+setSlide(currentSlide);
