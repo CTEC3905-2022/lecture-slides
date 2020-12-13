c5edc8161ef2941e9ce1b0b3ada9145608539d08
Graeme Stuart <ggstuart@gmail.com>
2020-12-13 11:16:38 +0000

    move entry point code into config.js and add new config options.

--- js/config.js
+++ js/config.js
@@ -1,20 +1,47 @@
-doSkipHello = JSON.parse(localStorage['skipHello']);
-console.log(!!doSkipHello);
-skipHello.checked = doSkipHello;
+showHello.checked = !JSON.parse(localStorage.getItem('skipHello'));
+mapOn.checked = JSON.parse(localStorage.getItem('map'));
+fullOn.checked = JSON.parse(localStorage.getItem('full'));
 
 window.addEventListener('storage', ev => {
 	console.log(ev);
 });
 
-if(!doSkipHello) {
-	config.classList.add('active');
-}
-
 dismiss.addEventListener('click', ev => {
 	config.classList.remove('active');
-	localStorage['skipHello'] = JSON.stringify(skipHello.checked);
+	localStorage['skipHello'] = JSON.stringify(!showHello.checked);
+	localStorage['full'] = JSON.stringify(fullOn.checked);
+	localStorage['map'] = JSON.stringify(mapOn.checked);
 });
 
 configToggler.addEventListener('click', ev => {
 	config.classList.toggle('active');
+});
+
+fullOn.addEventListener('click', ev => {
+	setFull(fullOn.checked);
+});
+mapOn.addEventListener('click', ev => {
+	setMap(mapOn.checked);
+});
+
+
+loadSlides(filename).then(container => {
+	hljs.initHighlighting();
+	slides = container.querySelectorAll('section');
+	slides.item(0).classList.add('current');
+	setSlide(parseInt(currentSlide));
+	slides.forEach((slide, slide_number) => {
+		slide.addEventListener('click', ev => {
+			setSlide(slide_number);
+		})
+	})
+	if(showHello.checked) {
+		config.classList.add('active');
+	}
+	if(mapOn.checked) {
+		toggleMap();
+	}
+	if(fullOn.checked) {
+		toggleFull();
+	}
 });
--- js/slides.js
+++ js/slides.js
@@ -4,20 +4,11 @@ let queryParams = new URLSearchParams(window.location.search);
 let currentSlide = queryParams.has('slide') ? queryParams.get('slide') : 0;
 let filename = queryParams.has('file') ? queryParams.get('file') : 'index.md';
 let slides;
+let touchX;
 
-loadSlides(filename).then(container => {
-	hljs.initHighlighting();
-	slides = container.querySelectorAll('section');
-	slides.item(0).classList.add('current');
-	setSlide(parseInt(currentSlide));
-	slides.forEach((slide, slide_number) => {
-		slide.addEventListener('click', ev => {
-			setSlide(slide_number);
-		})
-	})
-});
 
 function setSlide(slide_number) {
+	currentSlide = slide_number;
 	let previous = document.querySelector('#slideDeck section.current');
 	let candidate = slides.item(slide_number);
 	previous.classList.remove('current');
@@ -26,6 +17,7 @@ function setSlide(slide_number) {
 	queryParams.set('slide', slide_number);
 	window.history.replaceState({}, "", `${window.location.origin}${window.location.pathname}?${queryParams.toString()}`);
 	if(slideDeck.classList.contains('map')) {
+		slideDeck.focus();
 		candidate.scrollIntoView(false);
 	}
 }
@@ -33,17 +25,44 @@ function setSlide(slide_number) {
 prev.addEventListener('click', prevSlide);
 next.addEventListener('click', nextSlide);
 
-function prevSlide(ev) {
+function prevSlide() {
 	slideDeck.classList.add('backwards');
 	currentSlide--;
 	if(currentSlide < 0) { currentSlide = slides.length - 1; }
 	setSlide(currentSlide);
 }
-function nextSlide(ev) {
+function nextSlide() {
 	slideDeck.classList.remove('backwards');
 	currentSlide++;
 	if(currentSlide >= slides.length) { currentSlide = 0; }
 	setSlide(currentSlide);
+}
+
+function toggleMap() {
+	slideDeck.classList.toggle('map');
+	if(slideDeck.classList.contains('map')) {
+		document.querySelector('#slideDeck section.current').scrollIntoView(false);
+	}
+}
+
+function setMap(value) {
+	if(value) {
+		slideDeck.classList.add('map');
+		document.querySelector('#slideDeck section.current').scrollIntoView(false);
+	} else {
+		slideDeck.classList.remove('map');
+	}
+}
+
+function toggleFull() {
+	document.body.classList.toggle('clean');
+}
+function setFull(value) {
+	if(value) {
+		document.body.classList.add('clean');
+	} else {
+		document.body.classList.remove('clean');
+	}
 }
 
 document.addEventListener('keydown', ev => {
@@ -55,10 +74,10 @@ document.addEventListener('keydown', ev => {
 			prevSlide();
 			break;
 		case "f":
-			document.body.classList.toggle('clean');
+			toggleFull();
 			break;
 		case "m":
-			slideDeck.classList.toggle('map');
+			toggleMap();
 			break;
 		case "Enter":
 			if(slideDeck.classList.contains('map')) {
@@ -68,7 +87,6 @@ document.addEventListener('keydown', ev => {
 	}
 });
 
-let touchX;
 document.addEventListener('touchstart', ev => {
 	touchX = ev.touches[0].clientX;
 });
